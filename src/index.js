b90e0f8b533b84731b313ccc31359b5da5215f7d
User Name <artur.it2914@gmail.com>
2020-02-14 15:28:58 +0200

    Add: all

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
-const { TaskController, UserController } = require( './controllers' );
-const { checkAuthorization, comparePassword } = require( './middlewares/user' );
 const express = require( 'express' );
-
+const router = require( './router' );
 const PORT = process.env.NODE_ENV || 3000;
 
 const app = express();
 app.use( express.json() );
-
-app.post( '/login', comparePassword );
-
-app.post( '/user', UserController.createUser );
-app.get( '/user/:id', UserController.getUserById );
-app.patch( '/user/:id', UserController.updateUserById );
-app.delete( '/user/:id', UserController.deleteUserById );
-
-app.use( checkAuthorization );
-
-app.post( '/Task', TaskController.createTask );
-app.get( '/Task/:id', TaskController.getTaskById );
-app.patch( '/Task/:id', TaskController.updateTaskById );
-app.delete( '/Task/:id', TaskController.deleteTaskById );
-
+app.use( router );
 app.listen( PORT, () => {
   console.log( `Example app listening on port ${PORT}!` );
 } );
+
+
+
--- src/middlewares/user/extractUserPK.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  req.userId = req.params.id || req.query.id || req.body.id;
-  if (req.userId) {
-    return next();
-  }
-};
